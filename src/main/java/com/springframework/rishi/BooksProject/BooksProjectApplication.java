ad0760b8733bd1d4d67c00dd6a0383279f7ff73c
rishi237 <rishirajsrj@gmail.com>
2020-07-30 23:53:30 +0530

    initial check in

---
+++ src/main/java/com/springframework/rishi/BooksProject/BooksProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.rishi.BooksProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/rishi/BooksProject/BooksProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.rishi.BooksProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
